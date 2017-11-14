74decaa22ff2c8384cd17ea3547ac3c31bcb8120
Josh Steverman <jstever@umich.edu>
2017-11-14 10:47:28 -0500

    helper script to dump a list of raw enumchron strings

---
+++ scripts/get_series_ec_strings.rb
@@ -0,0 +1,21 @@
+require 'registry/registry_record'
+require 'registry/source_record'
+require 'dotenv'
+SourceRecord = Registry::SourceRecord
+RegistryRecord = Registry::RegistryRecord
+#takes a text file with an oclc on each line, 
+#and spits out the raw enumchron strings.
+
+Dotenv.load!
+
+oclcs = open(ARGV.shift).read.split("\n").map {|o| o.to_i}
+
+Mongoid.load!(File.expand_path("../config/mongoid.yml", __FILE__), :production)
+SourceRecord.where(oclc_resolved:{"$in":oclcs},
+                   deprecated_timestamp:{"$exists":0}).each do |src|
+  src.extract_enum_chron_strings.each do |ec|
+    puts ec
+  end
+end
+
+  
